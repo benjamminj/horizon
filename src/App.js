566d078333435848306691d36329a89ae166cbd3
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-01-23 20:58:06 -0800

    Initial footer component setup

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 
 import Header from './Header'
 import Main from './Main'
+import Footer from './Footer'
 
 class App extends Component {
   render () {
@@ -10,7 +11,7 @@ class App extends Component {
       <div>
         <Header />
         <Main />
-        <footer>Footer</footer>
+        <Footer />
       </div>
     )
   }
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Footer.css'
+
+class Footer extends Component {
+  render () {
+    return (
+      <footer>Footer</footer>
+    )
+  }
+}
+
+export default Footer
