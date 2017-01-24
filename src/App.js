bb5f7144a3c5fc722286d8a2f7fb0e8c4c0a2bc5
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-01-23 20:29:54 -0800

    Basic site setup / configuration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <header>Header</header>
+        <main>Main</main>
+        <footer>Footer</footer>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
