138c77870aab47a0aae0a458a8b35546b958a6b3
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-01-25 14:31:38 -0800

    Made initial API call

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,31 @@
+import 'whatwg-fetch'
+
 import React, { Component } from 'react'
 import './Main.css'
 
+import { toUTC } from './Utils'
+
+const { fetch } = window
+
 class Main extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {}
+  }
+
+  componentDidMount () {
+    fetch('http://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&formatted=0')
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          sunrise: new Date(json.results.sunrise)
+        })
+      })
+  }
+
   render () {
     return (
-      <main>Main</main>
+      <main>{ JSON.stringify(this.state.sunrise, null, 4) }</main>
     )
   }
 }
