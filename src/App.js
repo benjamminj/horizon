f783f2a5e7f859494045f4f0bee742019c79db52
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-01-26 02:23:03 -0800

    Rename folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import './App.css'
 
-import Header from './Components/Header'
-import Main from './Components/Main'
-import Footer from './Components/Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 class App extends Component {
   render () {
