d98411ed392f040fbfed5f1b53156c41a6fce5a6
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-01-27 22:17:44 -0800

    Prep to deploy

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 import express from 'express'
+import bodyParser from 'body-parser'
 import proxy from 'express-http-proxy'
 
 const app = express()
 
-app.use(express.static('build'))
+app.all('/', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api/sunrise-sunset/lat=:lat&lng=:lng', proxy('api.sunrise-sunset.org/json', {
   forwardPathAsync: (req, res) => {
     return new Promise((resolve, reject) => {
+      console.log(req.headers)
       const { lat, lng } = req.params
 
       resolve(`http://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&formatted=0`)
