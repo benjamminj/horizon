fc44e06c46bd8be52a2755b14b3eb4b8670c4473
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-01-27 16:27:55 -0800

    Debugging http issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import 'whatwg-fetch'
+import 'fetchp'
 
 import { toUTC } from './Utils'
 import './App.css'
@@ -8,7 +9,7 @@ import Header from './components/Header'
 import Clock from './components/Clock'
 import Footer from './components/Footer'
 
-const { fetch } = window
+// const { fetchJsonp } = window
 
 class App extends Component {
   constructor (props) {
@@ -37,30 +38,32 @@ class App extends Component {
   }
 
   async getSunriseSunsetTimes (lat, lng) {
-    const res = await fetch(`//api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&formatted=0`)
-    console.log(res)
-    const { results } = await res.json()
+    try {
+      const res = await window.fetch(`//api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&formatted=0`).then(res => res.json())
+      console.log(res)
+      const { results } = await res
 
+      this.setState({
+        civilTwilightBegin: toUTC(new Date(results.civil_twilight_begin)),
+        civilTwilightEnd: toUTC(new Date(results.civil_twilight_end)),
+        sunrise: toUTC(new Date(results.sunrise)),
+        sunset: toUTC(new Date(results.sunset)),
+        now: toUTC(new Date(Date.now())),
+        dayLength: results.day_length * 1000
+      })
 
-    this.setState({
-      civilTwilightBegin: toUTC(new Date(results.civil_twilight_begin)),
-      civilTwilightEnd: toUTC(new Date(results.civil_twilight_end)),
-      sunrise: toUTC(new Date(results.sunrise)),
-      sunset: toUTC(new Date(results.sunset)),
-      now: toUTC(new Date(Date.now())),
-      dayLength: results.day_length * 1000
-    })
-
-    this.getTimeLeft()
+      this.getTimeLeft()
+    } catch (err) {
+      console.log('BOOOOO', err)
+    }
   }
 
   async getTimesForLocation () {
     try {
-      const res = await fetch('//ip-api.com/json?callback=?')
-      console.log(res)
-      const { lat, lon } = await res.json()
+      const res = await window.fetch('https://freegeoip.net/json/').then(res => res.json())
+      const { latitude, longitude } = await res
 
-      await this.getSunriseSunsetTimes(lat, lon)
+      await this.getSunriseSunsetTimes(latitude, longitude)
     } catch (err) {
       console.log('BOOOOO', err)
     }
