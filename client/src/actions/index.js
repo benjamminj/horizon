fb8c99dad099603f638172b296df57f7b9a4d4a3
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-01-28 14:36:17 -0800

    Add actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,15 @@
+import { ADD_LOCATION, ADD_SUNRISE_SUNSET_RESULTS } from './actionTypes'
+
+export const addLocation = (location) => {
+  return {
+    type: ADD_LOCATION,
+    location
+  }
+}
+
+export const addSunriseSunsetResults = (results) => {
+  return {
+    type: ADD_SUNRISE_SUNSET_RESULTS,
+    results
+  }
+}
