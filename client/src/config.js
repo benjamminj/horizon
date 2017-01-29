603ad81a1cb6dc3dadcf36a0bff6459f215b3ca1
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-01-28 20:39:14 -0800

    Update server location for prod

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
-// export const API_SERVER = '//benjaminjohnson-sunrise-sunset.herokuapp.com'
+export const API_SERVER = '//benjaminjohnson-sunrise-sunset.herokuapp.com'
 
-export const API_SERVER = 'http://localhost:8080'
+// export const API_SERVER = 'http://localhost:8080'
 
 export default { API_SERVER }
