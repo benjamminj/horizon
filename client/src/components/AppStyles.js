fe20c7537864f62dbdb7e5987fb40dd73546a688
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-01-31 01:21:11 -0800

    Clean up AppStyles

--- client/src/components/AppStyles.js
+++ client/src/components/AppStyles.js
@@ -1,10 +1,4 @@
 /* eslint-disable */
-const NIGHT = [0, 0, 0]
-const RED = [255, 0, 0]
-
-const BLUE = [0, 0, 255]
-const PURPLE = blend(RED, BLUE, 0.5)
-
 function blend (color1, color2, percent) {
   return color1.map((val, i) => Math.floor((val * (1 - percent)) + (color2[i] * percent)))
 }
@@ -17,12 +11,9 @@ function generateBackground (color1, color2, color3) {
 }
 
 export default (status, times) => {
-  const { isDay } = status // eslint-disable-line
   const { isLoading, loadSuccess } = times
 
-  // colors need to be a radial gradient
-
   return {
-    background: !isLoading && loadSuccess ? `rgb(${BLUE})` : `rgb(${RED})`
+    background: !isLoading && loadSuccess ? `darkgreen` : `black`
   }
 }
