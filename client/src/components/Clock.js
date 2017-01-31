1597d1fcedbf929689ff03250e97798ec69947cf
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-01-31 01:19:37 -0800

    Clean up component

--- client/src/components/Clock.js
+++ client/src/components/Clock.js
@@ -3,22 +3,23 @@ import './Clock.css'
 
 class Clock extends Component {
   componentDidMount () {
-    const { getTimeLeft, times, status } = this.props
+    const { getTimeLeft, status, times } = this.props
 
     getTimeLeft(status, times)
   }
 
   componentDidUpdate () {
-    const { increaseCount, times, status } = this.props
+    const { increaseCount, status, times } = this.props
 
     increaseCount(status, times)
   }
 
   render () {
-    const { timeLeft } = this.props.times
-    const { hours, minutes, seconds } = this.formatTimeDisplay()
+    const { status, times } = this.props
 
-    const isDay = this.props.status
+    const { isDay } = status
+    const { timeLeft } = times
+    const { hours, minutes, seconds } = this.formatTimeDisplay()
 
     const timeDisplay = timeLeft ? <h1>{hours}:{minutes}:{seconds}</h1> : <h1 />
 
@@ -32,11 +33,12 @@ class Clock extends Component {
 
   formatTimeDisplay () {
     const { timeLeft } = this.props.times
-    const { padNumber } = this
 
     const hours = timeLeft / (1000 * 60 * 60)
     const minutes = (hours % 1) * 60
     const seconds = (minutes % 1) * 60
+
+    const { padNumber } = this
 
     return {
       hours: padNumber(Math.floor(hours)),
@@ -50,17 +52,13 @@ class Clock extends Component {
   }
 }
 
-const { bool, number, func, object } = PropTypes
+const { func, object } = PropTypes
 
 Clock.propTypes = {
-  timeLeft: number,
-  now: number,
-  increaseCount: func,
-  isLoading: bool.isRequired,
-  loadSuccess: bool.isRequired,
   getTimeLeft: func.isRequired,
-  times: object.isRequired,
-  status: object.isRequired
+  increaseCount: func,
+  status: object.isRequired,
+  times: object.isRequired
 }
 
 export default Clock
