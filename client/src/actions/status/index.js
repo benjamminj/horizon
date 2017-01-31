55b43cad02991cc15131f69b862e4b69dfd466c5
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-01-31 01:44:56 -0800

    Add change Light Level to status export

--- client/src/actions/status/index.js
+++ client/src/actions/status/index.js
@@ -1,5 +1,7 @@
 import * as actions from './actionTypes'
 import getIsDayAction from './getIsDay'
+import changeLightLevelAction from './changeLightLevel'
 
 export const actionTypes = actions
 export const getIsDay = getIsDayAction
+export const changeLightLevel = changeLightLevelAction
