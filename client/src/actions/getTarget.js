41e06f22b14b5a378e4d71f3388a0cc01f6824a1
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-01 15:54:16 -0800

    Change times structure -- add getTarget action

---
+++ client/src/actions/getTarget.js
@@ -0,0 +1,13 @@
+const GET_CURRENT_TARGET = 'GET_CURRENT_TARGET'
+
+const targetIndexes = {
+  sunrise: 3,
+  sunset: 5
+}
+
+export default (status) => {
+  return {
+    type: GET_CURRENT_TARGET,
+    target: /^waiting_/.test(status) ? targetIndexes[status.split('_')[1]] : null
+  }
+}
