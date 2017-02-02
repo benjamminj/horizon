c27369dd774b3f8e7f43e6cfc6c79ccf94015900
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-02 00:47:26 -0800

    create api Request functions

--- client/src/actions/async/apiRequests.js
+++ client/src/actions/async/apiRequests.js
@@ -2,13 +2,32 @@
 import fetch from 'isomorphic-fetch'
 import { SUNRISE_SUNSET_API, GEOLOCATION_API } from '../urls' // eslint-disable-line
 
-export const reqSunriseSunsetAPI = async (lat, lng) => {
+async function apiRequest (url) {
+  try {
+    const json = await fetch(url).then((res) => res.json())
+
+    return json
+  } catch (err) {
+    throw new Error(err)
+  }
+}
+
+export async function reqSunriseSunsetAPI (lat, lng) {
   const today = new Date(Date.now())
   const date = [today.getFullYear(), today.getMonth() + 1, today.getDate()]
 
   try {
-    const res = await fetch(`${SUNRISE_SUNSET_API}/api/sunrise-sunset/lat=${lat}&lng=${lng}&date=${date.join('-')}`)
-    const json = await res.json()
+    const json = await apiRequest(`${SUNRISE_SUNSET_API}/api/sunrise-sunset/lat=${lat}&lng=${lng}&date=${date.join('-')}`)
+
+    return json
+  } catch (err) {
+    return err
+  }
+}
+
+export async function reqGeolocation () {
+  try {
+    const json = await apiRequest(GEOLOCATION_API)
 
     return json
   } catch (err) {
