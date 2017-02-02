2c49c4e3012f8e09f5f065a548101d3842622955
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-01 17:54:23 -0800

    Clear our actions index.js

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,17 +1,1 @@
-import { ADD_LOCATION, ADD_SUNRISE_SUNSET_RESULTS } from './actionTypes'
 
-export const addLocation = (location) => {
-  return {
-    type: ADD_LOCATION,
-    location
-  }
-}
-
-export const addSunriseSunsetResults = (results) => {
-  return {
-    type: ADD_SUNRISE_SUNSET_RESULTS,
-    results
-  }
-}
-
-export default { addLocation, addSunriseSunsetResults }
