d8348c77760aac186c3495e7627ce99a9377f3c1
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-01 17:29:34 -0800

    Modifying reducers

--- client/src/reducers/breakpoints.js
+++ client/src/reducers/breakpoints.js
@@ -3,10 +3,7 @@ import { GET_BREAKPOINTS } from '../actions/getBreakpoints.js'
 export default (state = [], action) => {
   switch (action.type) {
     case GET_BREAKPOINTS:
-      return {
-        ...state,
-        ...action.breakpoints
-      }
+      return action.breakpoints
     default:
       return state
   }
--- client/src/reducers/status.js
+++ client/src/reducers/status.js
@@ -1,27 +1,27 @@
-import { actionTypes } from '../actions/status'
+// import { actionTypes } from '../actions/status'
 
-const {
-  GET_IS_DAY,
-  CHANGE_LIGHT_LEVEL
-} = actionTypes
+// const {
+//   GET_IS_DAY,
+//   CHANGE_LIGHT_LEVEL
+// } = actionTypes
 
-export default (state = {}, action) => {
-  switch (action.type) {
-    case GET_IS_DAY:
-      const { isDay } = action
+// export default (state = {}, action) => {
+//   switch (action.type) {
+//     case GET_IS_DAY:
+//       const { isDay } = action
 
-      return {
-        ...state,
-        isDay
-      }
-    case CHANGE_LIGHT_LEVEL:
-      const { lightLevel } = action
+//       return {
+//         ...state,
+//         isDay
+//       }
+//     case CHANGE_LIGHT_LEVEL:
+//       const { lightLevel } = action
 
-      return {
-        ...state,
-        lightLevel
-      }
-    default:
-      return state
-  }
-}
+//       return {
+//         ...state,
+//         lightLevel
+//       }
+//     default:
+//       return state
+//   }
+// }
