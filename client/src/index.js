5362580242aa3ff9804e17f513ba1d846bd52cf0
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-03 16:08:33 -0800

    Clean up

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,10 @@
+/* eslint-disable */
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 import { Provider } from 'react-redux'
 import AppContainer from './containers/AppContainer'
 import './index.css'
-
-import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
