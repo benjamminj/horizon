420fb3b1e860f2460878e63729b780c4490ce682
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-07 16:39:18 -0800

    Adjust to env vars and set prod SERVER

--- client/src/actions/async/apiRequests.js
+++ client/src/actions/async/apiRequests.js
@@ -1,5 +1,4 @@
 import fetch from 'isomorphic-fetch'
-import { SUNRISE_SUNSET_API, GEOLOCATION_API } from '../constants/urls'
 
 async function apiRequest (url) {
   try {
@@ -15,13 +14,15 @@ export async function reqSunriseSunsetAPI ({ lat, lng }, date) {
   const formattedDate = [date.getFullYear(), date.getMonth() + 1, date.getDate()]
 
   try {
-    const json = await apiRequest(`${SUNRISE_SUNSET_API}/api/sunrise-sunset/lat=${lat}&lng=${lng}&date=${formattedDate.join('-')}`)
+    const json = await apiRequest(`${process.env.REACT_APP_SERVER}/api/sunrise-sunset/lat=${lat}&lng=${lng}&date=${formattedDate.join('-')}`)
 
     return json
   } catch (err) {
     return err
   }
 }
+
+const GEOLOCATION_API = '//freegeoip.net/json/'
 
 export async function reqGeolocationAPI () {
   try {
--- client/src/actions/constants/urls.js
+++
@@ -1,2 +0,0 @@
-export const SUNRISE_SUNSET_API = process.env.REACT_APP_SERVER
-export const GEOLOCATION_API = '//freegeoip.net/json/'
