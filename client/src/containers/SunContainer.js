987e948dff0c4e6ef8dfa272872af1f149186332
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-07 23:54:32 -0800

    Bugfix -- allow for ascending and descending night versions

--- client/src/containers/SunContainer.js
+++ client/src/containers/SunContainer.js
@@ -6,20 +6,21 @@ const mapStateToProps = ({ breakpoints, currentIndex, remaining }, ownProps) => 
   const current = breakpoints[currentIndex]
   const next = (currentIndex !== breakpoints.length - 1) ? breakpoints[currentIndex + 1] : breakpoints[0]
 
-
   const timeBetween = next.time - current.time
-
-  console.log(timeBetween - (next.time - Date.now()))
   const timeToNext = timeBetween - (next.time - Date.now())
 
   const percentToNext = timeToNext / timeBetween
   const distanceToNext = next.lightLevel * percentToNext
   const percent = next.lightLevel > current.lightLevel ? (next.lightLevel - distanceToNext) : (current.lightLevel - distanceToNext)
 
-  const nightLevelGoneHeight = 40
+  const nightLevelGoneHeight = 20
   const isNight = percent < nightLevelGoneHeight
   const nightDistance = Math.abs(next.lightLevel - current.lightLevel)
-  const nightLevel = isNight ? 1 - ((nightDistance * percentToNext) / nightLevelGoneHeight) : 0
+
+  const base = next.lightLevel > current.lightLevel ? current.lightLevel : next.lightLevel
+  const nightLevel = isNight ? 1 - ((base * percentToNext) / nightLevelGoneHeight) : 0
+
+  console.log((base * percentToNext) / nightLevelGoneHeight)
 
   return {
     percent,
