6cedf16a9512adbfd93cf4f4ebb8ce518a3a1486
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-08 02:44:37 -0800

    Convert to stateless functional component

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Footer.css'
 
-class Footer extends Component {
-  render () {
-    return (
-      <footer>
-        <h6>Sunrise and sunset times made available courtesy of <a href='//sunrise-sunset.org/' target='_blank'>sunrise-sunset.org</a></h6>
-      </footer>
-    )
-  }
-}
+const Footer = () => (
+  <footer>
+    <h6>Sunrise and sunset times made available courtesy of <a href='//sunrise-sunset.org/' target='_blank'>sunrise-sunset.org</a></h6>
+  </footer>
+)
 
 export default Footer
