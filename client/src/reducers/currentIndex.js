1f772204a034586e98f44639d0ad2120e61daf9b
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-07 23:53:35 -0800

    Store

--- client/src/reducers/currentIndex.js
+++ client/src/reducers/currentIndex.js
@@ -1,8 +1,10 @@
-import { GET_CURRENT_INDEX } from '../actions/constants/actionTypes'
+import { GET_CURRENT_INDEX, INC_CURRENT_INDEX } from '../actions/constants/actionTypes'
 
 export default (state = null, action) => {
   switch (action.type) {
     case GET_CURRENT_INDEX:
+      return action.currentIndex
+    case INC_CURRENT_INDEX:
       return action.currentIndex
     default:
       return state
