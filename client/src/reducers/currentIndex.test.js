f015b753d948a5d282eb6713b04048e8d0a99bbe
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-09 12:30:50 -0800

    Write tests for currentIndex reducer

---
+++ client/src/reducers/currentIndex.test.js
@@ -0,0 +1,34 @@
+import { GET_CURRENT_INDEX, INC_CURRENT_INDEX } from '../actions/constants/actionTypes'
+import currentIndexReducer from './currentIndex.js'
+
+const state = 2
+
+const getCurrentIndexAction = {
+  type: GET_CURRENT_INDEX,
+  currentIndex: 5
+}
+
+const incCurrentIndexAction = {
+  type: INC_CURRENT_INDEX,
+  currentIndex: 3
+}
+
+const unknownAction = {
+  type: 'UNKNOWN'
+}
+
+test('should have default state of `null`', () => {
+  expect(currentIndexReducer(undefined, unknownAction)).toBeNull()
+})
+
+test('should return previous state if action is unknown', () => {
+  expect(currentIndexReducer(state, unknownAction)).toBe(2)
+})
+
+test('should return `5` if action === GET_CURRENT_INDEX', () => {
+  expect(currentIndexReducer(state, getCurrentIndexAction)).toBe(5)
+})
+
+test('should return `3` if action === INC_CURRENT_INDEX', () => {
+  expect(currentIndexReducer(state, incCurrentIndexAction)).toBe(3)
+})
