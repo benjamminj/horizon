ac38ee5e6e30dde113255ce5684e8bf656ea022c
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-09 12:51:19 -0800

    Add tests for breakpoints reducer / formatting

--- client/src/reducers/breakpoints.js
+++ client/src/reducers/breakpoints.js
@@ -1,7 +1,4 @@
-import {
-  GET_BREAKPOINTS,
-  UPDATE_AM_BREAKPOINTS
-} from '../actions/constants/actionTypes'
+import { GET_BREAKPOINTS, UPDATE_AM_BREAKPOINTS } from '../actions/constants/actionTypes'
 
 export default (state = [], action) => {
   switch (action.type) {
