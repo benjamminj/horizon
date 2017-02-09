f83aad4ef0eda992f93bfc087c229198facf2ae1
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-09 11:43:55 -0800

    Formatting -- no changes

--- client/src/reducers/location.js
+++ client/src/reducers/location.js
@@ -1,15 +1,11 @@
-import {
-  GET_LOCATION
-} from '../actions/constants/actionTypes'
+import { GET_LOCATION } from '../actions/constants/actionTypes'
 
 export default (state = {}, action) => {
   switch (action.type) {
     case GET_LOCATION:
       return {
         ...state,
-        ...action.location,
-        isLoading: false,
-        loadSuccess: true
+        ...action.location
       }
     default:
       return state
