fb9ab61ac032d9c79622f946a572d88b87c299d1
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-09 19:17:03 -0800

    Bugfix -- throws error on load

--- client/src/actions/getCurrentIndex.js
+++ client/src/actions/getCurrentIndex.js
@@ -1,15 +1,16 @@
 import { GET_CURRENT_INDEX } from './constants/actionTypes'
 
 export default (breakpoints, now) => {
+  const lastIndex = breakpoints.length - 1
+
   const currentIndex = breakpoints.findIndex(({ time }, i, arr) => {
-    const { length } = arr
-    const next = i < length - 1 ? arr[i + 1] : arr[length - 1]
+    const next = i < lastIndex ? arr[i + 1] : arr[lastIndex]
 
     return (time <= now) && (now < next.time)
   })
 
   return {
     type: GET_CURRENT_INDEX,
-    currentIndex: currentIndex > 0 ? currentIndex : (breakpoints.length - 1)
+    currentIndex: currentIndex >= 0 ? currentIndex : lastIndex
   }
 }
