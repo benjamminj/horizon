1acd481e788059aa58ebf7df4dfec9b4aa46ded0
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-09 19:34:58 -0800

    Clean up -- remove actions that are no longer needed

--- client/src/actions/getBreakpoints.js
+++ client/src/actions/getBreakpoints.js
@@ -1,36 +1,9 @@
+// TODO -- move to async folder
+
 import { reqSunriseSunsetAPI } from './async/apiRequests'
 import { formatBreakpoints } from './utils'
 
-import {
-  GET_BREAKPOINTS,
-  GET_BREAKPOINTS_FAIL,
-  GET_BREAKPOINTS_REQUEST,
-  GET_BREAKPOINTS_SUCCESS
-} from './constants/actionTypes'
-
-// Status methods will be routed to the status reducer
-function getBreakpointsRequest () {
-  return {
-    type: GET_BREAKPOINTS_REQUEST,
-    loading: true
-  }
-}
-
-function getBreakpointsFail (err) {
-  return {
-    type: GET_BREAKPOINTS_FAIL,
-    loading: false,
-    err
-  }
-}
-
-function getBreakpointsSuccess () {
-  return {
-    type: GET_BREAKPOINTS_SUCCESS,
-    loading: false,
-    success: true
-  }
-}
+import { GET_BREAKPOINTS } from './constants/actionTypes'
 
 function getBreakpoints (breakpoints) {
   return {
@@ -41,17 +14,14 @@ function getBreakpoints (breakpoints) {
 
 export default (location) => {
   return async (dispatch) => {
-    dispatch(getBreakpointsRequest())
-
     try {
       const today = new Date(Date.now())
       const { results } = await reqSunriseSunsetAPI(location, today)
       const breakpoints = formatBreakpoints(results)
 
-      dispatch(getBreakpointsSuccess())
       return dispatch(getBreakpoints(breakpoints))
     } catch (err) {
-      dispatch(getBreakpointsFail(err))
+      throw new Error(err)
     }
   }
 }
