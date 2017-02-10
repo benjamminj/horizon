c521f4e148a39866b47f339857ee95e6f0ae83e9
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-09 19:38:18 -0800

    Cleanup -- remove actions that are no longer being used

--- client/src/actions/getLocation.js
+++ client/src/actions/getLocation.js
@@ -1,26 +1,6 @@
 import { reqGeolocationAPI } from './async'
 
-import {
-  GET_LOCATION_REQUEST,
-  GET_LOCATION_FAIL,
-  GET_LOCATION_SUCCESS,
-  GET_LOCATION
-} from './constants/actionTypes'
-
-function getLocationRequest () {
-  return {
-    type: GET_LOCATION_REQUEST,
-    loading: true
-  }
-}
-
-function getLocationFail (err) {
-  return {
-    type: GET_LOCATION_FAIL,
-    loading: false,
-    err
-  }
-}
+import { GET_LOCATION } from './constants/actionTypes'
 
 function getLocation (location) {
   return {
@@ -29,25 +9,13 @@ function getLocation (location) {
   }
 }
 
-function getLocationSuccess () {
-  return {
-    type: GET_LOCATION_SUCCESS,
-    loading: false,
-    success: true
-  }
-}
-
 export default () => {
   return async (dispatch) => {
-    dispatch(getLocationRequest())
-
     try {
       const { latitude: lat, longitude: lng } = await reqGeolocationAPI()
-
-      dispatch(getLocationSuccess())
       return dispatch(getLocation({ lat, lng }))
     } catch (err) {
-      dispatch(getLocationFail(err))
+      throw new Error(err)
     }
   }
 }
