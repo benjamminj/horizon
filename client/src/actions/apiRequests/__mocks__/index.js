cae81d3eeb86b24ffbd2f824ae61cfcd4ec34faa
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-02-10 22:55:08 -0800

    mock reqSunriseSunsetAPI function

---
+++ client/src/actions/apiRequests/__mocks__/index.js
@@ -0,0 +1,26 @@
+export const reqSunriseSunsetAPI = (location, today) => {
+  return new Promise((resolve, reject) => {
+    if (location) {
+
+      const baseDate = 1486794165218 // Feb 10, 2017 @ 10:23pm PST in UTC
+      const minute = 1000 * 60
+
+      const results = {
+        astronomical_twilight_begin: new Date(baseDate),
+        nautical_twilight_begin: new Date(baseDate + (minute * 30)),
+        civil_twilight_begin: new Date(baseDate + (minute * 60)),
+        sunrise: new Date(baseDate + (minute * 90)),
+        solar_noon: new Date(baseDate + (minute * 60 * 4)),
+        day_length: 50000000,
+        sunset: new Date(baseDate + (minute * 60 * 7)),
+        civil_twilight_end: new Date(baseDate + (minute * 60 * 7.5)),
+        nautical_twilight_end: new Date(baseDate + (minute * 60 * 8)),
+        astronomical_twilight_end: new Date(baseDate + (minute * 60 * 9))
+      }
+
+      resolve({ results })
+    } else {
+      reject(new Error('error fetching sunrise and sunset data'))
+    }
+  })
+}
